4d7f6e4c9061e30fdfb325f1463fab3ef3ec8da9
vickreiner <77959513+vickreiner@users.noreply.github.com>
2022-01-05 08:41:41 -0800

    Added flow cytometry helper

---
+++ FlowCytometry analysis helper/facs_helper_call.sh
@@ -0,0 +1,5 @@
+############    --------   FACS analysis helper call    --------    ############
+
+Rscript /Users/vkreiner/Desktop/Scripts/facs_analysis_helper.R --inputcsv  "~/exp005_raw_results.csv" --metaElements "Prestimulation_EXCLUDE_timepoint_hIgG-stim"
+
+
